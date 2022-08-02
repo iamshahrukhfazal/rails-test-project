b059e374af57ac66602d8029da1f8db8030cf387
imshahrukh <sk902224@gmail.com>
2022-08-02 22:58:45 +0500

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # before_action :authenticate_user!
   before_action :set_post, only: %i[show update destroy]
   # before_action :get_post, only: %i[show]
-  skip_before_action :verify_authenticity_token, only: %i[search]
+  skip_before_action :verify_authenticity_token
   
   # GET /posts or /posts.json
   def index  
@@ -14,32 +14,18 @@ class PostsController < ApplicationController
       format.json {render json: @posts.map{ |post| post.attributes.merge({ comments:post.comments.count, likes:post.likes.count, data:post.content,email:post.user.email })}}
     end
   end
-
+                                       
   # GET /posts/1 or /posts/1.json
   def show
     # authorize Post
     respond_to do |format|
-      format.html
-      format.json {
-        # merge Commente
-        render json: @post.attributes.merge(
-          {
-            content:@post.content,
-            likes:@post.likes.count,
-            liked_by:@post.likes,
-            comments:@post.comments.where(reply_id: nil).order(id: :desc).map{ |comment|
-               comment.attributes.merge(
-                  { likes:comment.likes.count,
-                    liked_by:comment.likes,
-
-                    replies:comment.replies.map{|reply|
-                       reply.attributes.merge({likes:reply.likes.count,liked_by:reply.likes,content:reply.content}) },
-                       content:comment.content })}})}
+      format.html                
+      format.json {render json: get_data}
     end
   end
 
   # def edit; end
-
+                                                             
   def new
     authorize Post
     @post = current_user.posts.new
@@ -47,14 +33,16 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    authorize Post
+    # authorize Post
     @post = Post.new(post_params)
-    @post.user = current_user
+    # @post.user = current_user
     # refactor
     @post.status = :unpublished
 
     respond_to do |format|
       if @post.save
+        format.json {render json: @post}
+
         Rails.logger.debug 'save'
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -64,9 +52,13 @@ class PostsController < ApplicationController
   end
 
   def update
-    authorize Post
+    # authorize Post
+
     respond_to do |format|
-      format.html { redirect_to post_url(@post), notice: t('errors.update_post') } if @post.update(post_params)
+      if @post.update(post_params)
+          format.json {render json: @post}
+          format.html { redirect_to post_url(@post), notice: t('errors.update_post') } 
+      end
       format.js
     end
   end
@@ -97,6 +89,43 @@ class PostsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def post_params
-    params.require(:post).permit(:content, :links, :status, :title)
+    params.require(:post).permit(:content, :links, :status, :title,:user_id)
+  end
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  def get_data
+    @post.attributes.merge(
+      {
+        content:@post.content,
+        likes:@post.likes.count,
+        liked_by:@post.likes,
+        comments:@post.comments.where(reply_id: nil).order(id: :desc).map{ |comment|
+           comment.attributes.merge(
+              { likes:comment.likes.count,
+                liked_by:comment.likes,
+
+                replies:comment.replies.map{|reply|
+                   reply.attributes.merge({likes:reply.likes.count,liked_by:reply.likes,content:reply.content}) },
+                   content:comment.content })}})
   end
 end
